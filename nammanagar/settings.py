ee40187d576d8a1160926ceec3c0e13b63d8420e
thiyagj19 <thiyaguj19@live.com>
2024-08-01 21:30:00 +0530

    added static files

--- nammanagar/settings.py
+++ nammanagar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static") 
+STATIC_URL = "/staticfiles/" 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS += [
+    os.path.join(BASE_DIR, 'fonts'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

b6654ca8864a9daae241d3188dd534c320b07096
thiyagj19 <thiyaguj19@live.com>
2024-08-01 21:26:33 +0530

    modified build sh file

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-pip install -r requirements.txt
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --no-input
+echo "BUILD END"

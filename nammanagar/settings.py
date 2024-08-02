9cb3539e48ddb55700711c6166851cfb103352a5
thiyagj19 <thiyaguj19@live.com>
2024-08-02 07:16:21 +0530

    static images

--- nammanagar/settings.py
+++ nammanagar/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/var/www/static/",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

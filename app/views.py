59f77de1f3b1497d1255f13fdd99f7d5cb8c5533
thiyagj19 <thiyaguj19@live.com>
2024-08-03 22:13:00 +0530

    updated pages for nammanagar

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def nagarhome(request):
     context = {'text': 'This is some text to be rendered as HTML.'}
-    return render(request, 'nagarhome.html', context)
+    return render(request, 'index.html', context)

92170423b05edeb32a9a8e6253f187c3e3f531ea
thiyagj19 <thiyaguj19@live.com>
2024-08-05 22:01:42 +0530

    added new picture relief material

--- app/views.py
+++ app/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from random import randint
 
 def nagarhome(request):
-    picture = "ReliefMaterial-" + str(randint(1, 4)) + ".jpeg"
+    picture = "ReliefMaterial-" + str(randint(0, 4)) + ".jpeg"
     context = {'picture': picture}
     print("picture = ", picture)
     return render(request, 'Independence.html', context)
 
 def reliefhome(request):
-    picture = "ReliefMaterial-" + str(randint(1, 4)) + ".jpeg"
+    picture = "ReliefMaterial-" + str(randint(0, 4)) + ".jpeg"
     context = {'picture': picture}
     print("picture = ", picture)
     return render(request, 'Wayand.html', context)

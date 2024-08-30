3b16cf56d70d080fe4224431b909da98741f6f95
thiyagj19 <thiyaguj19@live.com>
2024-08-30 22:26:56 +0530

    wayand images from digitalocean

--- app/views.py
+++ app/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from random import randint
 
 def nagarhome(request):
+    bucket = "https://nammanagar.blr1.cdn.digitaloceanspaces.com/"
     picture = "ReliefMaterial-" + str(randint(0, 4)) + ".jpeg"
-    context = {'picture': picture}
+    context = {'picture': picture, 'bucket': bucket}
     print("picture = ", picture)
     return render(request, 'Independence.html', context)
 
 def reliefhome(request):
-    picture = "ReliefMaterial-" + str(randint(0, 4)) + ".jpeg"
+    picture = "https://nammanagar.blr1.cdn.digitaloceanspaces.com/wayand/" + "ReliefMaterial-" + str(randint(0, 4)) + ".jpeg"
     context = {'picture': picture}
     print("picture = ", picture)
     return render(request, 'Wayand.html', context)

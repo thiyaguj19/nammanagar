cf0a755d60627c97285db6894b8560c30d85d0d1
thiyagj19 <thiyaguj19@live.com>
2024-09-15 11:45:16 +0530

    home page modification

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path(r'', views.home, name='nagarhome'),
-    path(r'', views.chaturthi, name='chaturthi'),
+    path(r'chaturthi/', views.chaturthi, name='chaturthi'),
     path(r'iday/', views.idayhome, name='idayhome'),
     path(r'wayand/', views.reliefhome, name='reliefhome'),
 ]

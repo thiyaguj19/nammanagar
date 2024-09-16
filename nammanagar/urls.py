f732ef7582411f2930d220a16a0d2cd829d3932e
thiyagj19 <thiyaguj19@live.com>
2024-09-16 08:53:11 +0530

    url addition for admin page

--- nammanagar/urls.py
+++ nammanagar/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('', include('app.urls')),
     path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

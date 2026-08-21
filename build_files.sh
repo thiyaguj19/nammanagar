29bf644b76654ca71261e6b88212ee3d3174d39b
thiyagarajan mohan <thiyaguj19@live.com>
2026-08-21 15:31:24 +0530

    added few more changes

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ echo "BUILD START"
 
 # Use python3 to match Vercel's active runtime environment
 python3 -m pip install -r requirements.txt
-python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
 
 echo "BUILD END"

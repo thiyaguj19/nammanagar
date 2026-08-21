4a1cc6266f4c4857e8442318c74cd22cdf6dcc87
thiyagarajan mohan <thiyaguj19@live.com>
2026-08-21 11:39:45 +0530

    build file change

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigrations app
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --no-input
 echo "BUILD END"

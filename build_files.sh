6de94aead61771a3ff16107a1398208c1fcaed96
thiyagarajan mohan <thiyaguj19@live.com>
2026-08-21 11:21:58 +0530

    added make migrations

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
+python3.9 manage.py makemigrations app
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --no-input
 echo "BUILD END"

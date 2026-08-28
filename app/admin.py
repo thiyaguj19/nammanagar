52aaebdf04a302b507976bcbafd58e98e142ffb3
thiyagarajan mohan <thiyaguj19@live.com>
2026-08-28 19:57:27 +0530

    added JapamCompletionAdmin

--- app/admin.py
+++ app/admin.py
@@ -7,3 +7,9 @@ admin.site.register(Sponsors)
 admin.site.register(SponsoredBy)
 admin.site.register(WalkLog)
 admin.site.register(JapamCompletion)
+
+@admin.register(JapamCompletion)
+class JapamCompletionAdmin(admin.ModelAdmin):
+    list_display = ('chant_type', 'created_at', 'duration_seconds')
+    list_filter = ('chant_type', 'created_at')
+    date_hierarchy = 'created_at'
